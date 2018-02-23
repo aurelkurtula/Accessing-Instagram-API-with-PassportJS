fcb283d3e2876cbf0b032e39a04b984a0cbd0027
marginal <aurelsmarginalia@gmail.com>
2018-02-23 12:38:10 +0000

    .

--- authentication/strategies.js
+++ authentication/strategies.js
@@ -3,10 +3,10 @@ import Instagram from 'passport-instagram';
 
 export function instagram(){
   const InstagramStrategy = Instagram.Strategy; 
-  
+
   passport.use(new InstagramStrategy({
-    clientID: "5f5511f7c59e4d729513461ac1f783cd",
-      clientSecret: "08aedf89a0694378af5e98e6b7e8945c",
+    clientID: "...",
+      clientSecret: "...",
       callbackURL: "http://localhost:3000/auth/instagram/callback"
   }, (accessToken, refreshToken, profile, done) => {
       let user = {};
